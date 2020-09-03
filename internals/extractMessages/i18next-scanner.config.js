b8dd79d3853d2ebd078fc83a76e16d6f88113fe9
Can Sahin <cann2005@gmail.com>
2020-09-03 22:00:30 +0300

    added i18next-scanner for extracting messages

---
+++ internals/extractMessages/i18next-scanner.config.js
@@ -0,0 +1,58 @@
+var fs = require('fs');
+const path = require('path');
+const typescript = require('typescript');
+const compilerOptions = require('../../tsconfig.json').compilerOptions;
+
+const stringfyTranslationObjects = require('./stringfyTranslations.js');
+
+module.exports = {
+  input: [
+    'src/app/**/**.{ts,tsx}',
+    '!**/node_modules/**',
+    '!src/app/**/*.test.{ts,tsx}',
+  ],
+  output: './',
+  options: {
+    debug: false,
+    removeUnusedKeys: false,
+    func: {
+      list: ['t'],
+      extensions: [''], // We dont want this extension because we manually check on transform function below
+    },
+    lngs: ['en', 'de'],
+    defaultLng: 'en',
+    defaultNs: 'translation',
+    resource: {
+      loadPath: 'src/locales/{{lng}}/{{ns}}.json',
+      savePath: 'src/locales/{{lng}}/{{ns}}.json',
+      jsonIndent: 2,
+      lineEnding: '\n',
+    },
+    keySeparator: '.', // char to separate keys
+    nsSeparator: ':', // char to split namespace from key
+    interpolation: {
+      prefix: '{{',
+      suffix: '}}',
+    },
+  },
+  transform: function transform(file, enc, done) {
+    const extensions = ['.ts', '.tsx'];
+
+    const { base, ext } = path.parse(file.path);
+    if (extensions.includes(ext) && !base.includes('.d.ts')) {
+      const content = fs.readFileSync(file.path, enc);
+      parseContent(content, this.parser);
+    }
+
+    done();
+  },
+};
+function parseContent(content, parser) {
+  const { outputText } = typescript.transpileModule(content, {
+    compilerOptions: compilerOptions,
+  });
+  const cleanedContent = stringfyTranslationObjects(outputText);
+  parser.parseFuncFromString(cleanedContent);
+}
+
+module.exports.parseContent = parseContent;
