de3c44837ce69b9762f4b653659272dcc135cd3b
Caleb Hoff <crunchycat1@gmail.com>
2022-08-21 18:06:35 -0500

    minor: remove redundant ../

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -10,7 +10,7 @@ import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import { GlobalStyle } from '../styles/global-styles';
+import { GlobalStyle } from 'styles/global-styles';
 
 import { HomePage } from './pages/HomePage/Loadable';
 import { NotFoundPage } from './pages/NotFoundPage/Loadable';
